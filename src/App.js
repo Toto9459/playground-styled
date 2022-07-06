c82b3185bdf0f3ef26fdc16f8807de44d6d60efb
Toni <toni.busatta@unkle.fr>
2022-07-06 17:41:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input';
+import InputUnkle from './components/InputUnkle';
+import styled from 'styled-components'
+
+
+const Wrap = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrap>
+      <Input />
+      <InputUnkle />
+    </Wrap>
+    
   );
 }
 
